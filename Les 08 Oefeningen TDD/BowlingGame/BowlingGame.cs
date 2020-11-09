f416fedd17c32561b5ec8c6fd100c63905f1d89f
janvandenpoelatAP <jan.vandenpoel@ap.be>
2020-11-09 12:42:48 +0100

    Bowling Game: stap 1.1"

---
+++ Les 08 Oefeningen TDD/BowlingGame/BowlingGame.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingGameScore
+{
+    public class BowlingGame
+    {
+    }
+}
---
+++ Les 08 Oefeningen TDD/BowlingGameTests/BowlingGameScoreTests.cs
@@ -0,0 +1,19 @@
+using BowlingGameScore;
+using NUnit.Framework;
+
+namespace BowlingGameScoreTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CanCreateGame()
+        {
+            var game = new BowlingGame();
+        }
+    }
+}
